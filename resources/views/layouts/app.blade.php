4d3c313774c017ccd31446af695a10526def7dc1
Jerrod Long <jlong@doeanderson.com>
2018-02-20 04:40:25 -0500

    [update] Page title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }} {{ app()->version() }}</title>
+        <title>Handyman | A Laravel and Vue Experiment</title>
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
